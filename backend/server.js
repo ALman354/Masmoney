93d7d672fbe862c3ef6e7faf3359d0fbca9e709f
Masmoney <nirusnamla@gmail.com>
2026-05-24 16:49:20 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+
+const db = require("./database/db");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// route test
+app.get("/", (req,res)=>{
+    res.json({
+        status:"success",
+        message:"API Finance berjalan 🚀"
+    });
+});
+
+
+
+// tambah transaksi
+app.post("/transaksi", (req,res)=>{
+
+    const {tipe, nominal, keterangan} = req.body;
+
+    const sql = `
+    INSERT INTO transaksi
+    (tipe, nominal, keterangan)
+    VALUES (?, ?, ?)
+    `;
+
+    db.query(
+        sql,
+        [tipe, nominal, keterangan],
+        (err,result)=>{
+
+            if(err){
+                return res.status(500).json({
+                    status:"error",
+                    message:err.message
+                });
+            }
+
+            res.json({
+                status:"success",
+                message:"Transaksi berhasil ditambah"
+            });
+
+        }
+    );
+
+});
+
+// ambil semua transaksi
+app.get("/transaksi",(req,res)=>{
+
+    const sql="SELECT * FROM transaksi ORDER BY id DESC";
+
+    db.query(sql,(err,result)=>{
+
+        if(err){
+            return res.status(500).json({
+                status:"error",
+                message:err.message
+            });
+        }
+
+        res.json(result);
+
+    });
+
+});
+
+
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`Server berjalan di port ${PORT}`);
+
+});
