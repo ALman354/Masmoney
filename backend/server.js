07efa3882c16b957f6c87300b8ec9c27d3dab91a
ALmanz <244848547+ALman354@users.noreply.github.com>
2026-05-29 15:43:34 +0800

    ganti koneksi database

--- backend/server.js
+++ backend/server.js
@@ -80,8 +80,7 @@ app.get("/transaksi",(req,res)=>{
 
 });
 
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`Server berjalan di port ${PORT}`);
 
